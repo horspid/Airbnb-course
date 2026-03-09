aa4c11c3513a7f994402313c3ae723ab61c76c42
horspid <horspid@yandex.ru>
2026-03-09 11:10:11 +0500

    upadted ReactRouter mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,40 @@
 import { createBrowserRouter, RouterProvider } from 'react-router';
 
 // Components
-import Footer from './components/Footer';
-import Header from './components/Header/Header'
+
 
 // Pages
 import Home from './pages/Home';
 import Listing from './pages/Listing';
 import Item from './pages/Item';
+import Layout from './layouts/Layout';
 
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		Component: Layout,
+		children: [
+			{
+				index: true,
+				Component: Home,
+			},
+			{
+				path: 'listing',
+				Component: Listing,
+			},
+			{
+				path: 'item',
+				Component: Item
+			}
+		]
+	}
+])
 
 
 function App() {
-
-	const router = createBrowserRouter([
-		{
-			path: '/',
-			element: <Home />
-		},
-		{
-			path: '/listing',
-			element: <Listing />
-		},
-		{
-			path: '/item',
-			element: <Item />
-		}
-	])
-
 	return (
-		<div>
-			<Header />
-			<RouterProvider router={router} />
-			{/* <Footer /> */}
-		</div>
+		<RouterProvider router={router} />
 	);
 }
 
---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router';
+import Footer from '@components/Footer';
+import Header from '@components/Header/Header'
+
+
+const Layout = () => {
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    );
+}
+
+export default Layout;
